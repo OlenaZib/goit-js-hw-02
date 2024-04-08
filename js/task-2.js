8c97f84eb2eb50bb9b83c314add7a9b46f9ec33c
OlenaZib <155896018+OlenaZib@users.noreply.github.com>
2024-04-08 22:16:49 +0200

    upd

---
+++ js/task-2.js
@@ -0,0 +1,9 @@
+function formatMessage(message, maxLength) {
+    if (message <= maxLength) {
+        return message;
+    }
+    else {
+        
+        return message.slice(0, maxLength) + "...";
+    }
+}
